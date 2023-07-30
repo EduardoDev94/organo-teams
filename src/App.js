d571637f9b3a70dd75f9ec86746f8e2a4fb80908
Eduardo Sena <dudu94bhz@gmail.com>
2023-07-30 18:24:30 -0300

    Criando componente Time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
 import { useState } from 'react';
+import Time from './components/Time';
 
 
 function App() {
@@ -13,6 +14,12 @@ const [jogadores, setJogadores] = useState([])
     <div className="App">
       <Banner/>
       <Formulario aoJogadorCadastrado={jogador => aoNovoJogadorCadastrado(jogador)}/>
+      <Time nome="Real Madrid" />
+      <Time nome="Barcelona" />
+      <Time nome="Milan"/>
+      <Time nome="Bayern de Munique"/>
+      <Time nome="Inter de Milão" />
+      <Time nome="Cruzeiro" />
     </div>
   );
 }
---
+++ src/components/Time/index.js
@@ -0,0 +1,12 @@
+import './Time.css'
+const time = (props) =>{
+  
+    return (
+        <section className='time'>
+            <h3>{props.nome}</h3>
+
+        </section>
+    )
+}
+
+export default Time
